99a7514d5fe73b911a4a93d315902b1a7df3a939
shakuu <todorov.h@gmail.com>
2016-10-23 18:42:41 +0300

    FileSystemProvider, decorated with missing directory creation

--- ComputersFactory/ComputersFactory.Data.FileSystem/FileSystemProviders/ResolveMissingPathFileSystemProvider.cs
+++ ComputersFactory/ComputersFactory.Data.FileSystem/FileSystemProviders/ResolveMissingPathFileSystemProvider.cs
@@ -6,8 +6,20 @@ using ComputersFactory.Data.FileSystem.Contracts;
 
 namespace ComputersFactory.Data.FileSystem.FileSystemProviders
 {
-    public class FileSystemProvider : IFileSystemProvider
+    public class ResolveMissingPathFileSystemProvider : IFileSystemProvider
     {
+        private readonly IFileSystemProvider fileSystemProviderComponent;
+
+        public ResolveMissingPathFileSystemProvider(IFileSystemProvider fileSystemProviderComponent)
+        {
+            if (fileSystemProviderComponent == null)
+            {
+                throw new ArgumentNullException(nameof(fileSystemProviderComponent));
+            }
+
+            this.fileSystemProviderComponent = fileSystemProviderComponent;
+        }
+
         public IEnumerable<string> ReadAllLines(string filePath)
         {
             if (string.IsNullOrEmpty(filePath))
@@ -21,7 +33,7 @@ namespace ComputersFactory.Data.FileSystem.FileSystemProviders
                 throw new FileNotFoundException(filePath);
             }
 
-            var content = File.ReadAllLines(filePath);
+            var content = this.fileSystemProviderComponent.ReadAllLines(filePath);
             return content;
         }
 
@@ -43,10 +55,10 @@ namespace ComputersFactory.Data.FileSystem.FileSystemProviders
             var containingDirectoryExists = Directory.Exists(directoryPath);
             if (!containingDirectoryExists)
             {
-                throw new DirectoryNotFoundException(filePath);
+                Directory.CreateDirectory(directoryPath);
             }
 
-            File.WriteAllText(filePath, text);
+            this.fileSystemProviderComponent.WriteAllText(filePath, text);
         }
     }
 }
