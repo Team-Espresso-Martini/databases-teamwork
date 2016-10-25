548c78ff134ed8220b372e1c8828cb05990d15ff
shakuu <todorov.h@gmail.com>
2016-10-25 17:09:57 +0300

    ComputersRepo

---
+++ ComputersFactory/ComputersFactory.Data.Repositories/Repositories/ComputersRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+using ComputersFactory.Data.Repositories.Repositories.Contracts;
+using ComputersFactory.Models;
+
+namespace ComputersFactory.Data.Repositories.Repositories
+{
+    public class ComputersRepository : IComputersRepository
+    {
+        public ComputersRepository(DbContext context)
+        {
+
+        }
+
+        public void Add(Computer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Computer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Computer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Computer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
