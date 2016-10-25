13d870afafc918343a33328295024d0856a52913
shakuu <todorov.h@gmail.com>
2016-10-25 10:22:42 +0300

    IRepository
    Get(id)
    Add(entity)
    Remove(entity)

---
+++ ComputersFactory/ComputersFactory.Data.Repositories/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ComputersFactory.Data.Repositories.Repositories.Contracts
+{
+    public interface IRepository<TEntity>
+        where TEntity : class
+    {
+        TEntity Get(int id);
+
+        void Add(TEntity entity);
+
+        void Remove(TEntity entity);
+    }
+}
