606025ce71d0c26046cae5f56f1cc9e397534f12
shakuu <todorov.h@gmail.com>
2016-10-25 10:27:03 +0300

    GenericRepository : IRepository

---
+++ ComputersFactory/ComputersFactory.Data.Repositories/Repositories/GenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using ComputersFactory.Data.Repositories.Repositories.Contracts;
+
+namespace ComputersFactory.Data.Repositories.Repositories
+{
+    public class GenericRepository<TEntity> 
+        : IRepository<TEntity> where TEntity : class
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
