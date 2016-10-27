8f33737bb54465e1cf370ef27c5b7ca7d2505ba5
pspassova <petya.spassova@abv.bg>
2016-10-27 17:39:24 +0300

    Added MongoDB writer.

---
+++ ComputersFactory/ComputersFactory.MongoDbWriter/MongoDbWriter.cs
@@ -0,0 +1,25 @@
+﻿using ComputersFactory.Models.Components;
+using MongoDB.Driver;
+using System;
+
+namespace ComputersFactory.MongoDbWriter
+{
+    public static class MongoDbWriter
+    {
+        public static IMongoDatabase Write()
+        {
+            var connectionString = "mongodb://localhost";
+            var dbName = "ComputersFactory";
+            var computersFactoryDb = GetDatabase(connectionString, dbName);
+
+            return computersFactoryDb;        
+        }
+
+        private static IMongoDatabase GetDatabase(string connectionString, string dbName)
+        {
+            var client = new MongoClient(connectionString);
+
+            return client.GetDatabase(dbName);
+        }
+    }
+}
