7569bdd3176a5f8a8eecc0e3c8fadfd6e8dc140b
shakuu <todorov.h@gmail.com>
2016-11-03 00:29:46 +0200

    added IModelConverter

---
+++ ComputersFactory/ComputersFactory.Data.SalesReports/Converters/Contracts/IModelConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ComputersFactory.Data.SalesReports.Converters.Contracts
+{
+    public interface IModelConverter
+    {
+        IEnumerable<TModelOut> Convert<TModelIn, TModelOut>(IEnumerable<TModelIn> inputData);
+    }
+}
