cd031f34ecfc5204d4a268a36de4fe74d2437e17
shakuu <todorov.h@gmail.com>
2016-11-02 19:22:50 +0200

    up

---
+++ ComputersFactory/ComputersFactory.Data.SalesReports.Generator/XmlGenerators/SalerReportsXmlGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ComputersFactory.Data.SalesReports.Generator.XmlGenerators.Contracts;
+using ComputersFactory.Models;
+
+namespace ComputersFactory.Data.SalesReports.Generator.XmlGenerators
+{
+    public class SalerReportsXmlGenerator : IXmlGenerator<SalesReport>
+    {
+        public void GenererateXmlFiles(IEnumerable<SalesReport> data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
