a2cb3b0bb04000b78e7a4461e54611daa9afe7aa
shakuu <todorov.h@gmail.com>
2016-11-02 17:06:45 +0200

    added SalesReportGenerator

---
+++ ComputersFactory/ComputersFactory.Data.SalesReports.Generator/DataGenerators/SalesReportGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ComputersFactory.Data.SalesReports.Generator.DataGenerators.Abstract;
+using ComputersFactory.Data.SalesReports.Generator.DataGenerators.Contracts;
+using ComputersFactory.Models;
+
+namespace ComputersFactory.Data.SalesReports.Generator.DataGenerators
+{
+    public class SalesReportGenerator : DataGenerator, ISalesReportGenerator
+    {
+        private readonly ISaleGenerator saleGenerator;
+
+        public SalesReportGenerator(ISaleGenerator saleGenerator)
+        {
+            if (saleGenerator == null)
+            {
+                throw new ArgumentNullException(nameof(saleGenerator));
+            }
+
+            this.saleGenerator = saleGenerator;
+        }
+
+        public IEnumerable<SalesReport> GenerateReports(int reportsCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
