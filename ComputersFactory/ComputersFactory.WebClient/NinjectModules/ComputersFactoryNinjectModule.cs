27d4397edcd4c0fa8be0d96d6badc078f4527d0e
shakuu <todorov.h@gmail.com>
2016-11-04 23:39:25 +0200

    created web client

---
+++ ComputersFactory/ComputersFactory.WebClient/NinjectModules/ComputersFactoryNinjectModule.cs
@@ -0,0 +1,19 @@
+﻿using Ninject.Modules;
+using Ninject.Extensions.Conventions;
+
+using System.IO;
+using System.Reflection;
+
+namespace ComputersFactory.WebClient.NinjectModules
+{
+    public class ComputersFactoryNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Kernel.Bind(ctx =>
+                 ctx.FromAssembliesInPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                 .SelectAllClasses()
+                 .BindDefaultInterface());
+        }
+    }
+}
---
+++ ComputersFactory/ComputersFactory.WebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ComputersFactory.WebClient.Startup))]
+namespace ComputersFactory.WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
