828ad0daef2588fc97bb8393a055eef720d7e980
shakuu <todorov.h@gmail.com>
2016-11-05 17:46:12 +0200

    created SalesReportsCompressedExcelDataParser

---
+++ ComputersFactory/ComputersFactory.Data.SalesReports.Excel/CompressedExcelDataParsers/Contracts/IExcelZipProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ComputersFactory.Data.SalesReports.Excel.CompressedExcelDataParsers.Contracts
+{
+    public interface ICompressedExcelDataParser<TModel>
+    {
+        IEnumerable<TModel> ParseCompressedExcelData(string fileName);
+    }
+}
---
+++ ComputersFactory/ComputersFactory.Data.SalesReports.Excel/CompressedExcelDataParsers/SalesReportsCompressedExcelDataParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ComputersFactory.Data.SalesReports.Excel.CompressedExcelDataParsers.Contracts;
+using ComputersFactory.Data.SalesReports.Excel.ExcelDataReaders.Contracts;
+using ComputersFactory.Data.SalesReports.Excel.ExcelDataParsers.Contracts;
+
+using ComputersFactory.Models;
+
+namespace ComputersFactory.Data.SalesReports.Excel.CompressedExcelDataParsers
+{
+    public class SalesReportsCompressedExcelDataParser : ICompressedExcelDataParser<SalesReport>
+    {
+        private readonly IExcelDataReaderProvider readerProvider;
+        private readonly IExcelDataParser<SalesReport> dataParser;
+
+        public SalesReportsCompressedExcelDataParser(IExcelDataReaderProvider readerProvider, IExcelDataParser<SalesReport> dataParser)
+        {
+            if (readerProvider == null)
+            {
+                throw new ArgumentNullException(nameof(readerProvider));
+            }
+
+            if (dataParser == null)
+            {
+                throw new ArgumentNullException(nameof(dataParser));
+            }
+
+            this.readerProvider = readerProvider;
+            this.dataParser = dataParser;
+        }
+
+        public IEnumerable<SalesReport> ParseCompressedExcelData(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
