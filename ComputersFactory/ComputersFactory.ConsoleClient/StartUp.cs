0ab8fb83d1a4cb91d6f4fce5b703befd23621d45
pspassova <petya.spassova@abv.bg>
2016-11-05 07:15:32 +0200

    Data is seeded into MongoDB using IRepository.

--- ComputersFactory/ComputersFactory.ConsoleClient/StartUp.cs
+++ ComputersFactory/ComputersFactory.ConsoleClient/StartUp.cs
@@ -5,6 +5,10 @@ using ComputersFactory.Data.Migrations;
 using ComputersFactory.Data.MongoDbWriter;
 using ComputersFactory.Data.Repositories.UnitsOfWork;
 using ComputersFactory.Data.TransferToSql;
+using MongoDB.Driver;
+using ComputersFactory.Data.Repositories.Repositories.Base;
+using ComputersFactory.Data.MongoDbWriter.Models.Components;
+using ComputersFactory.Data.MongoDbWriter.Models;
 
 namespace ComputersFactory.ConsoleClient
 {
@@ -17,8 +21,30 @@ namespace ComputersFactory.ConsoleClient
             var db = new ComputersFactoryDbContext();
             var worker = new ComputersFactoryUnitOfWork(db);
 
-            //MongoDbWriter.GenerateData();
             //MongoToSqlMigrator.TransferData();
+
+            string dbHost = "mongodb://localhost";
+            string dbName = "ComputersFactory";
+
+            var client = new MongoClient(dbHost);
+            var database = client.GetDatabase(dbName);
+
+            var hardDrivesRepository = new GenericMongoRepository<HardDriveMongoModel>(database);
+            var memoriesRepository = new GenericMongoRepository<MemoryMongoModel>(database);
+            var motherboardsRepository = new GenericMongoRepository<MotherboardMongoModel>(database);
+            var processorsRepository = new GenericMongoRepository<ProcessorMongoModel>(database);
+            var videoCardsRepository = new GenericMongoRepository<VideoCardMongoModel>(database);
+            var computerShopsRepository = new GenericMongoRepository<ComputerShopMongoModel>(database);
+            var computersRepository = new GenericMongoRepository<ComputerMongoModel>(database);
+
+            var writer = new MongoDbWriter();
+            writer.GenerateHardDrives(hardDrivesRepository);
+            writer.GenerateMemories(memoriesRepository);
+            writer.GenerateMotherboards(motherboardsRepository);
+            writer.GenerateProcessors(processorsRepository);
+            writer.GenerateVideoCards(videoCardsRepository);
+            writer.GenerateComputerShops(computerShopsRepository);
+            writer.GenerateComputers(hardDrivesRepository, memoriesRepository, motherboardsRepository, processorsRepository, videoCardsRepository, computerShopsRepository, computersRepository);
         }
     }
 }
