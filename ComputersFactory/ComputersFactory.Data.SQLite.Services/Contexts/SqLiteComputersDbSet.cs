183d78674c56c21b3e48f1347c4d8a5506249150
shakuu <todorov.h@gmail.com>
2016-11-06 18:44:56 +0200

    dbSet implementation

--- ComputersFactory/ComputersFactory.Data.SQLite.Services/Contexts/SqLiteComputersDbSet.cs
+++ ComputersFactory/ComputersFactory.Data.SQLite.Services/Contexts/SqLiteComputersDbSet.cs
@@ -11,7 +11,8 @@ namespace ComputersFactory.Data.SQLite.Services.Contexts
 {
     public class SqLiteComputersDbSet : ISqLiteDbSet<SqLiteComputer>
     {
-        private const string ConnectionString = "Data Source=../../../../homework.sqlite;Version=3;";
+        private const string ConnectionString = @"Data Source=D:\TeamWorkFiles\SqLiteDb\sqlitedb.sqlite;Version=3;";
+        private const string TableName = "SqLiteComputers";
 
         private readonly IConnectionFactory connectionFactory;
         private readonly ICommandFactory commandFactory;
@@ -24,22 +25,99 @@ namespace ComputersFactory.Data.SQLite.Services.Contexts
 
         public void Add(SqLiteComputer entity)
         {
-            throw new NotImplementedException();
+            var query =
+                "INSERT INTO " + SqLiteComputersDbSet.TableName +
+                $" VALUES ({entity.Model}, {entity.Price}, {entity.Rating})";
+
+            var command = this.commandFactory.CreateSQLiteCommand(query);
+            var connection = this.connectionFactory.CreateSQLiteConnection(ConnectionString);
+            command.Connection = connection;
+
+            using (connection)
+            {
+                command.ExecuteNonQuery();
+            }
         }
 
         public IEnumerable<SqLiteComputer> All()
         {
-            throw new NotImplementedException();
+            var query =
+                "SELECT *" +
+                "FROM " + SqLiteComputersDbSet.TableName;
+
+            var command = this.commandFactory.CreateSQLiteCommand(query);
+            var connection = this.connectionFactory.CreateSQLiteConnection(ConnectionString);
+            command.Connection = connection;
+
+            using (connection)
+            {
+                var reader = command.ExecuteReader();
+
+                var result = new List<SqLiteComputer>();
+                while (reader.Read())
+                {
+                    var nextInstance = new SqLiteComputer();
+                    nextInstance.Id = (int)reader["Id"];
+                    nextInstance.Model = (string)reader["Model"];
+                    nextInstance.Price = (decimal)reader["Price"];
+                    nextInstance.Rating = (int)reader["Rating"];
+
+                    result.Add(nextInstance);
+                }
+
+                return result;
+            }
         }
 
         public void CreateTable()
         {
-            throw new NotImplementedException();
+            var query =
+                "CREATE TABLE SqLiteComputers( " +
+                    "Id INT PRIMARY KEY IDENTITY," +
+                    "Model NVARCHAR(50)," +
+                    "Price MONEY, " +
+                    "Rating INT" +
+                    ")";
+
+            var command = this.commandFactory.CreateSQLiteCommand(query);
+            var connection = this.connectionFactory.CreateSQLiteConnection(ConnectionString);
+            command.Connection = connection;
+
+            using (connection)
+            {
+                command.ExecuteNonQuery();
+            }
         }
 
         public SqLiteComputer Find(int id)
         {
-            throw new NotImplementedException();
+            var query =
+               "SELECT *" +
+               "FROM " + SqLiteComputersDbSet.TableName +
+               " WHERE Id = " + id.ToString();
+
+            var command = this.commandFactory.CreateSQLiteCommand(query);
+            var connection = this.connectionFactory.CreateSQLiteConnection(ConnectionString);
+            command.Connection = connection;
+
+            using (connection)
+            {
+                var reader = command.ExecuteReader();
+
+                var result = new List<SqLiteComputer>();
+                while (reader.Read())
+                {
+                    var nextInstance = new SqLiteComputer();
+                    nextInstance.Id = (int)reader["Id"];
+                    nextInstance.Model = (string)reader["Model"];
+                    nextInstance.Price = (decimal)reader["Price"];
+                    nextInstance.Rating = (int)reader["Rating"];
+
+                    result.Add(nextInstance);
+                }
+
+                return result.FirstOrDefault();
+            }
         }
     }
 }
