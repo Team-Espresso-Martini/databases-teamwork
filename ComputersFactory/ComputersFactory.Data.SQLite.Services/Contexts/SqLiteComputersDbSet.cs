57cc9db5d31f95098c365e5d9f675a29abbfb827
shakuu <todorov.h@gmail.com>
2016-11-06 18:28:56 +0200

    DbSet

---
+++ ComputersFactory/ComputersFactory.Data.SQLite.Services/Contexts/SqLiteComputersDbSet.cs
@@ -0,0 +1,45 @@
+﻿using ComputersFactory.Data.Models;
+using ComputersFactory.Data.SQLite.Services.Contexts.Contracts;
+using ComputersFactory.Data.SQLite.Services.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputersFactory.Data.SQLite.Services.Contexts
+{
+    public class SqLiteComputersDbSet : ISqLiteDbSet<SqLiteComputer>
+    {
+        private const string ConnectionString = "Data Source=../../../../homework.sqlite;Version=3;";
+
+        private readonly IConnectionFactory connectionFactory;
+        private readonly ICommandFactory commandFactory;
+
+        public SqLiteComputersDbSet(IConnectionFactory connectionFactory, ICommandFactory commandFactory)
+        {
+            this.connectionFactory = connectionFactory;
+            this.commandFactory = commandFactory;
+        }
+
+        public void Add(SqLiteComputer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SqLiteComputer> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqLiteComputer Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
