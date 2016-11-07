f9416012418ec33de6b25cf9df20d0ac851e0e80
shakuu <todorov.h@gmail.com>
2016-11-07 15:14:05 +0200

    DataAccess ConsoleApp

--- ComputersFactory/ComputersFactory.Data.MySql.OpenAccess.ConsoleApp/Program.cs
+++ ComputersFactory/ComputersFactory.Data.MySql.OpenAccess.ConsoleApp/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Telerik.OpenAccess;
 
 namespace ComputersFactory.Data.MySql.OpenAccess.ConsoleApp
 {
     public class Program
     {
-        public static void Main()
+        static void Main()
         {
+            UpdateDatabase();
+        }
+
+        private static void UpdateDatabase()
+        {
+            using (var context = new MySqlComputersContext.FluentModel())
+            {
+                var schemaHandler = context.GetSchemaHandler();
+                EnsureDB(schemaHandler);
+            }
+        }
+
+        private static void EnsureDB(ISchemaHandler schemaHandler)
+        {
+            string script = null;
+            if (schemaHandler.DatabaseExists())
+            {
+                script = schemaHandler.CreateUpdateDDLScript(null);
+            }
+            else
+            {
+                schemaHandler.CreateDatabase();
+                script = schemaHandler.CreateDDLScript();
+            }
+
+            if (!string.IsNullOrEmpty(script))
+            {
+                schemaHandler.ExecuteDDLScript(script);
+            }
         }
     }
+}
 }
